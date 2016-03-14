e01ae0c8bb24e9726d6c032926a4c498ef0edc1f
tlacy1914 <tlacy1914@yahoo.com>
2016-03-13 21:44:35 -0400

    wk 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
+#Sets and gets values for the matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+      
+      mx <- NULL
+      set <- function(y) {
+            x <<- y
+            mx <<- NULL
+      }
+      get <- function() x
+      setinv <- function(inverse) mx <<- inverse
+      getinv <- function() mx
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
+            
+      }
+      
+     
 
 
 ## Write a short comment describing this function
+##1st Checks for cached matrix 
+##Gets inverse of matrix, including non-square
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      mx <- x$getinv()
+      if(!is.null(mx)) {
+            message("getting cached data")
+            return(mx)
+      }
+      data <- x$get()
+      mx <- solve(crossprod(data, ...))
+      x$setinv(mx)
+      mx
 }
